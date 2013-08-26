15c226aeb622179fd54e2c6e803c50b053439dec
Andrew Forward <aforward@gmail.com>
2013-08-26 02:37:19 -0400

    Added support for redis 2.6

--- attributes/source.rb
+++ attributes/source.rb
@@ -18,12 +18,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+checksum = {
+  "2.4.18" => "7afdaede54ccd5b1a838776eb82b4c97fe7d5d17",
+  "2.6.14" => "fdf61c693e5c4908b4bb44c428d4a2b7568f05566c144c58fdf19c5cb12a9caf"
+}
+
 default['redis']['source']['version'] = "2.4.18"
 default['redis']['source']['prefix']  = "/usr/local"
 
-default['redis']['source']['tar_url']   =
-  "http://redis.googlecode.com/files/redis-#{node['redis']['source']['version']}.tar.gz"
-default['redis']['source']['tar_checksum']   = "7afdaede54ccd5b1a838776eb82b4c97fe7d5d17"
+default['redis']['source']['tar_url'] = "http://redis.googlecode.com/files/redis-#{node['redis']['source']['version']}.tar.gz"
+default['redis']['source']['tar_checksum']   = checksum[node['redis']['source']['version']]
 
 default['redis']['source']['create_service']  = true
 
